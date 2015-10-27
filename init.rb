160fee4d30010f321a28e4d74675a675e2b9f762
Scott Bellware <sbellware@gmail.com>
2015-10-27 13:27:36 -0500

    The libraries dir is added to the load path before the library is loaded

--- init.rb
+++ init.rb
@@ -3,10 +3,10 @@ require 'bundler/setup' unless ENV['DISABLE_BUNDLER'] == 'on'
 lib_dir = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
 
-require 'telemetry/errors'
-
 libraries_dir = ENV['LIBRARIES_DIR']
 unless libraries_dir.nil?
   libraries_dir = File.expand_path(libraries_dir)
   $LOAD_PATH.unshift libraries_dir unless $LOAD_PATH.include?(libraries_dir)
 end
+
+require 'telemetry/errors'
