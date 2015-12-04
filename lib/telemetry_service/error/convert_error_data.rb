ad143f4087f1a595ce5949d8f1d143a6e2bb9d40
Scott Bellware <sbellware@gmail.com>
2015-12-03 20:37:08 -0600

    Whitespace is standardized

--- lib/telemetry_service/error/convert_error_data.rb
+++ lib/telemetry_service/error/convert_error_data.rb
@@ -1,23 +1,23 @@
-# module TelemetryService
-#   module Error
-#     class ConvertErrorData
-#       def self.call(recorded_event)
-#         data = RaygunClient::Data.new
+module TelemetryService
+  module Error
+    class ConvertErrorData
+      def self.call(recorded_event)
+        data = RaygunClient::Data.new
 
-#         data.occurred_time = recorded_event.time
-#         data.machine_name = recorded_event.hostname
-#         data.client = RaygunClient::Data::ClientInfo.build
+        data.occurred_time = recorded_event.time
+        data.machine_name = recorded_event.hostname
+        data.client = RaygunClient::Data::ClientInfo.build
 
-#         raw_error_data = Casing::Underscore.(recorded_event.error)
+        raw_error_data = Casing::Underscore.(recorded_event.error)
 
-#         error_data = ErrorData.build(raw_error_data)
+        error_data = ErrorData.build(raw_error_data)
 
-#         data.error = error_data
+        data.error = error_data
 
-#         data.custom_data = { :error_id => recorded_event.error_id }
+        data.custom_data = { :error_id => recorded_event.error_id }
 
-#         data
-#       end
-#     end
-#   end
-# end
+        data
+      end
+    end
+  end
+end
