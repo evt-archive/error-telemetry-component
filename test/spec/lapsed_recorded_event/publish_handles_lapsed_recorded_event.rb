29165c7d2bd2a500e0dcfac05c204eb27651c1e7
Scott Bellware <sbellware@gmail.com>
2016-02-01 00:03:04 -0600

    Test for lapsed not posting to raygun

--- test/spec/lapsed_recorded_event/publish_handles_lapsed_recorded_event.rb
+++ test/spec/lapsed_recorded_event/publish_handles_lapsed_recorded_event.rb
@@ -2,22 +2,28 @@ require_relative '../spec_init'
 
 context "Publish" do
   context "Recorded event has lapsed" do
-    test do
-      # handler clock is set to time + lapse interval
-      # send recorded event
-      # writes NotPublished
-      # Note: projection: entity picks up the Lapsed time
-      # from the Lapsed event
 
 
-      recorded_event = ErrorTelemetryComponent::Controls::Messages::Recorded::Lapsed.example
+    recorded_event = ErrorTelemetryComponent::Controls::Messages::Recorded::Lapsed.example
 
-      now = ErrorTelemetryComponent::Controls::LapseTime.now
+    now = ErrorTelemetryComponent::Controls::LapseTime.now
 
-      substitute = [:clock, :writer, :raygun_post]
-      publish_error = ErrorTelemetryComponent::Controls::PublishError.example(substitute: substitute)
+    substitute = [:clock, :writer, :raygun_post]
+    publish_error = ErrorTelemetryComponent::Controls::PublishError.example(substitute: substitute)
 
-      publish_error.clock.now = now
+    publish_error.clock.now = now
+
+    writer = publish_error.writer
+
+    raygun_post = publish_error.raygun_post
+    raygun_sink = RaygunClient::HTTP::Post.register_telemetry_sink(raygun_post)
+
+    publish_error.(recorded_event)
+
+    test "Does not send the error to Raygun" do
+      assert raygun_sink do
+        posts.length == 0
+      end
     end
   end
 end
