1a3f796d71a84f3adf33ded085272c9e6ac987ad
Scott Bellware <sbellware@gmail.com>
2016-02-01 12:43:22 -0600

    WIP (Handoff to Nathan)

--- lib/error_telemetry_component/controls/raygun_data.rb
+++ lib/error_telemetry_component/controls/raygun_data.rb
@@ -1,9 +1,9 @@
 module ErrorTelemetryComponent
   module Controls
     module RaygunData
-      def self.example(custom_data=nil)
+      def self.example(custom_data=nil, time: nil)
         custom_data ||= { error_id: Controls::ID.example }
-        RaygunClient::Controls::Data.example(custom_data)
+        RaygunClient::Controls::Data.example(custom_data, time: time)
       end
     end
   end
