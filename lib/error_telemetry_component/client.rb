0ca7d9c5a312b530e863f00391085f7ab975befa
Nathan Ladd <nathan@obsidianexchange.com>
2016-02-08 09:10:31 -0600

    Move HostInfo to client namespace

--- lib/error_telemetry_component/client.rb
+++ lib/error_telemetry_component/client.rb
@@ -5,6 +5,7 @@ require 'error_data'
 require 'initializer'; Initializer.activate
 require 'serialize'
 
-require 'error_telemetry_component/host_info'
 require 'error_telemetry_component/messages/commands/record'
+
+require 'error_telemetry_component/client/host_info'
 require 'error_telemetry_component/client/record'
---
+++ lib/error_telemetry_component/client/host_info.rb
@@ -0,0 +1,27 @@
+module ErrorTelemetryComponent
+  module Client
+    class HostInfo
+      def hostname
+        self.class.hostname
+      end
+
+      def self.hostname
+        Socket.gethostname
+      end
+
+      def self.configure(receiver)
+        new.tap do |instance|
+          receiver.host_info = instance
+        end
+      end
+
+      module Substitute
+        NullHostInfo = Struct.new(:hostname)
+
+        def self.build
+          NullHostInfo.new
+        end
+      end
+    end
+  end
+end
