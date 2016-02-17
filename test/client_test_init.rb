bb5aca0de776381f2dc40e287c15a6c957e58aff
Scott Bellware <sbellware@gmail.com>
2016-02-17 12:49:28 -0600

    Notation of mistakes in file load order in init files

--- test/client_test_init.rb
+++ test/client_test_init.rb
@@ -1,3 +1,5 @@
+raise 'Why is all this code in here rather than in test_init?'
+
 ENV['CONSOLE_DEVICE'] ||= 'stdout'
 ENV['LOG_COLOR'] ||= 'on'
 
@@ -11,6 +13,7 @@ ENV['LOGGER'] ||= 'off'
 
 puts RUBY_DESCRIPTION
 
+raise 'Why is client init separate if every time test_init is loaded, this is loaded, which loads the client code?'
 require_relative '../client_init'
 
 require 'error_telemetry_component/client/controls'
